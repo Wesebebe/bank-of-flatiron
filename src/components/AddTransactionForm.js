47597faa14e6617845162efdd8e10ffdbfa3233e
+ <+>
2023-04-24 12:55:42 +0300

    Update App

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -21,8 +21,8 @@ function AddTransactionForm() {
     <div className="ui segment">
       <form onSubmit={handleSubmit} className="ui form">
         <div className="inline fields">
-          <input  value={date} onChange={(e) => setDate(e.target.value)} type="date" name="date" />
-          <input   value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" placeholder="Description" />
+          <input className="inputField" value={date} onChange={(e) => setDate(e.target.value)} type="date" name="date" />
+          <input className="ui inverted input"  value={description} onChange={(e) => setDescription(e.target.value)} type="text" name="description" placeholder="Description" />
           <input  value={category} onChange={(e) => setCategory(e.target.value)} type="text" name="category" placeholder="Category" />
           <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" name="amount" placeholder="Amount" step="0.01" />
         </div><br></br>
